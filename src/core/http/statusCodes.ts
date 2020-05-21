e7bed43ca938e1b222d9934eda2542bd2b20a00d
Warren Sadler <warrendsadler@gmail.com>
2020-05-21 09:00:16 -0500

    feat: :sparkles: add status-codes module

---
+++ src/core/http/statusCodes.ts
@@ -0,0 +1,13 @@
+/**
+ * @module core.http.statusCodes
+ *
+ * statusCodes provides a nominal collection of commonly used Http status
+ * codes for convenience and consistency.
+ */
+
+export const OK = 200;
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const NOT_FOUND = 404;
+export const INTERNAL_SERVER_ERROR = 500;
+export const NOT_IMPLEMENTED = 501;
